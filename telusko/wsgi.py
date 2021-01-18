96ebd729793c97c8021ac02b8d403477d2fb1be7
spartan289 <60476934+spartan289@users.noreply.github.com>
2021-01-18 17:54:53 +0530

    Update wsgi.py

--- telusko/wsgi.py
+++ telusko/wsgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
+import signal
+
+import sys
+import traceback
+
+import time
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'telusko.settings')
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
+
